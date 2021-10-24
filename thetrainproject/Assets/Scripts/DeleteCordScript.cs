19a80ddebae2defc8a132234559b69084fc628e4
DTsoukalos <51972234+DTsoukalos@users.noreply.github.com>
2021-10-24 11:08:29 +0300

    Introduce arrow

--- thetrainproject/Assets/Scripts/DeleteCordScript.cs
+++ thetrainproject/Assets/Scripts/DeleteCordScript.cs
@@ -6,68 +6,83 @@ using UnityEngine.UI;
 
 public class DeleteCordScript : MonoBehaviour
 {
+    //ακυρο το delete
+
+
     [SerializeField] Text setText;
     [SerializeField] Camera theCamera;
     [SerializeField] GameObject aImage;
     [SerializeField] GameObject aPointofinterest;
     [SerializeField] GameObject sphere;
 
+    [SerializeField] List<PointOfInterestScript> pointOfIntList = new List<PointOfInterestScript>();
+
     float angleNew=0f;
     float angleOld=0f;
     float angleDiference=0f;
- 
+
+    int listCounter = 0;
+
+    //var pointOfIntList;
+
+    //List<PointOfInterestScript> pointOfIntList = new List<PointOfInterestScript>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 frontCameraVector = new Vector2(sphere.transform.position.x - theCamera.transform.position.x,
-    sphere.transform.position.z - theCamera.transform.position.z);
 
-        Vector2 vectorToPontOfInterest = new Vector2(aPointofinterest.transform.position.x - theCamera.transform.position.x,
-            aPointofinterest.transform.position.z - theCamera.transform.position.z);
+        SetTheArrowImage();
+    }
+
+
+    public void SetTheArrowImage()
+    {
+        //front Camera Vector
+        Vector2 frontCameraVector = new Vector2(sphere.transform.position.x - theCamera.transform.position.x,
+sphere.transform.position.z - theCamera.transform.position.z);
+
+        //
+        Vector2 vectorToPontOfInterest = new Vector2(pointOfIntList[listCounter].GetPosition().x - theCamera.transform.position.x,
+            pointOfIntList[listCounter].GetPosition().z - theCamera.transform.position.z);
 
 
         angleNew = Vector2.SignedAngle(frontCameraVector, vectorToPontOfInterest);
-        angleDiference = angleNew- angleOld;
+        angleDiference = angleNew - angleOld;
         angleOld = angleNew;
 
         //Image Rotation
-        aImage.GetComponent<RectTransform>().Rotate(new Vector3(0,0,angleDiference));
+        aImage.GetComponent<RectTransform>().Rotate(new Vector3(0, 0, angleDiference));
 
-        setText.text =" angle "+ Vector2.SignedAngle(frontCameraVector,vectorToPontOfInterest)+
-            "angle diference" + angleDiference;
+        setText.text = " pointD " + pointOfIntList[listCounter].GetDistance() +
+            "cameraD" + Vector3.Distance(theCamera.transform.position, pointOfIntList[listCounter].GetPosition())
+            + " List Counter: " + listCounter + " Title: " + pointOfIntList[listCounter].GetTitle() 
+            +" testlistcount: "+ pointOfIntList.Count;
 
-        if (angleNew < 15)
+        if (Math.Abs (angleNew) < 14)
         {
+            
             aImage.SetActive(false);
+            if (pointOfIntList[listCounter].GetDidYouSeeIt() && listCounter< pointOfIntList.Count)
+            {
+
+                listCounter++;
+
+            }
         }
-        else {
+        if(pointOfIntList[listCounter].GetDistance() > Vector3.Distance(theCamera.transform.position, pointOfIntList[listCounter].GetPosition()))
+        {
             aImage.SetActive(true);
-                }
-        
+            pointOfIntList[listCounter].ResetScale();
+            pointOfIntList[listCounter].SetDidYouSeeIt();
+
+        }
     }
 
 
-    /**
- * Calculates the angle (in radians) between two vectors pointing outward from one center
- *
- * @param p0 first point
- * @param p1 second point
- * @param c center point
- */
-    public float find_angle(Vector3 p0,Vector3 p1,Vector3 c)
-    {
-        var p0c = Math.Sqrt(Math.Pow(c.x - p0.x, 2) +
-                            Math.Pow(c.y - p0.y, 2)); // p0->c (b)   
-        var p1c = Math.Sqrt(Math.Pow(c.x - p1.x, 2) +
-                            Math.Pow(c.y - p1.y, 2)); // p1->c (a)
-        var p0p1 = Math.Sqrt(Math.Pow(p1.x - p0.x, 2) +
-                             Math.Pow(p1.y - p0.y, 2)); // p0->p1 (c)
-        return (float)Math.Acos((p1c * p1c + p0c * p0c - p0p1 * p0p1) / (2 * p1c * p0c));
-    }
 }
--- thetrainproject/Assets/Scripts/PointOfInterestScript.cs
+++ thetrainproject/Assets/Scripts/PointOfInterestScript.cs
@@ -9,7 +9,17 @@ public class PointOfInterestScript : MonoBehaviour
     [SerializeField] string aTitle;
     [SerializeField] Texture theLandmarkImage;
     [SerializeField] string aDescription;
+    [SerializeField] float aDistance;
 
+    Vector3 position;
+
+    bool didYouSeeIt = false;
+
+    private void Start()
+    {
+        position = transform.position;
+        transform.localScale = new Vector3(0.00001f, 0.00001f, 0.00001f);
+    }
 
     public string GetTitle()
     {
@@ -24,5 +34,30 @@ public class PointOfInterestScript : MonoBehaviour
     public Texture GetLandmarkImage()
     {
         return theLandmarkImage;
+    }
+
+    public float GetDistance()
+    {
+        return aDistance;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return position;
+    }
+
+    public void ResetScale()
+    {
+        transform.localScale = new Vector3(20,20,20);
+    }
+
+    public bool GetDidYouSeeIt()
+    {
+        return didYouSeeIt;
+    }
+
+    public void SetDidYouSeeIt()
+    {
+        didYouSeeIt = true;
     }
 }
